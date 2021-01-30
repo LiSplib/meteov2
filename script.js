b004322ec8e93a594a00dfb4533f361dec1508bb
LiSplib <jcbarret@outlook.fr>
2021-01-30 17:41:38 +0100

    edit iphone test

--- script.js
+++ script.js
@@ -179,13 +179,10 @@
     })
 
     function formatDate(dateToFormat, option){
-       const formated = new Date(dateToFormat);
-        console.log(formated);
-        const goodDateFormat = new Intl.DateTimeFormat("fr-FR", option);
-        console.log(goodDateFormat);
-        goodDateFormat.format(formated);
-        console.log(goodDateFormat);
-        return goodDateFormat;
+        dateToFormat = new Date(dateToFormat);
+        console.log(dateToFormat);
+        dateToFormat = new Intl.DateTimeFormat("fr-FR", option).format(dateToFormat);
+        return dateToFormat;
     }
 
     main();
