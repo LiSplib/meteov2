72d7c9a1b30ab4052a93318670dcff804c1592fb
Jean-Christophe <52524271+LiSplib@users.noreply.github.com>
2021-08-09 16:42:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@
             .then(json => json.ip)
             .catch(err => handleError(err));
     
-            ville = await fetch('https://cors-anywhere.herokuapp.com/http://ip-api.com/json/' + ip)
+            ville = await fetch('http://ip-api.com/json/' + ip)
             .then(resultat => resultat.json())
             .then(json => json.city)
             .catch(err => handleError(err));
